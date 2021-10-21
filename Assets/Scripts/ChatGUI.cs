0cf70150084fb43f1bd14ead1988c5ee36839bc0
Nick <nickyrick@gmail.com>
2021-10-21 00:32:04 -0400

    Initialize Uno Project
    Initializing Uno project with all Unity project files.

---
+++ Assets/Scripts/ChatGUI.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+
+
+namespace Uno
+{
+    public class ChatGUI : MonoBehaviour
+    {
+        #region Private Fields
+
+        string connectedAs = "Connected as ";
+
+        #endregion
+
+        #region Private Serializable Fields
+
+        [Tooltip("The text that displays what username the player is currently connected as")]
+        [SerializeField]
+        private Text connectedAsText;
+
+        #endregion
+        // Start is called before the first frame update
+        void Start()
+        {
+            connectedAs += PlayerNameInputField.getUserName();
+            connectedAsText.text = connectedAs;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            connectedAsText.text = $"Connected as {PhotonNetwork.NickName}";
+        }
+    }
+}
+
---
+++ Assets/Scripts/PlayerNameInputField.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+using Photon.Pun;
+
+namespace Uno
+{
+    [RequireComponent(typeof(InputField))]
+    public class PlayerNameInputField : MonoBehaviour
+    {
+        #region Private Constants
+
+        const string playerNamePrefKey = "PlayerName";
+
+        #endregion
+
+        #region MonoBehavior Callbacks
+
+        void Start()
+        {
+            string defaultName = string.Empty;
+            InputField _inputField = this.GetComponent<InputField>();
+            if(_inputField != null)
+            {
+                if (PlayerPrefs.HasKey(playerNamePrefKey))
+                {
+                    defaultName = PlayerPrefs.GetString(playerNamePrefKey);
+                    _inputField.text = defaultName;
+                }
+            }
+
+            PhotonNetwork.NickName = defaultName;
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void SetPlayerName(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                Debug.Log("Player Name is null or empty");
+                return;
+            }
+            PhotonNetwork.NickName = value;
+
+            PlayerPrefs.SetString(playerNamePrefKey, value);
+        }
+
+        public static string getUserName()
+        {
+            if (PlayerPrefs.HasKey(playerNamePrefKey))
+            {
+                return PlayerPrefs.GetString(playerNamePrefKey);
+            }
+            else
+            {
+                return "anon";
+            }
+        }
+
+        #endregion
+    }
+}
