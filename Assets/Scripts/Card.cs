fe874b389184821793969fa162493958d748701f
Nick <nickyrick@gmail.com>
2021-11-09 19:09:13 -0500

    Combined repos

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class Card : MonoBehaviour
 {
+    public static int cards = 0;
     int focusZPosition = -100;
     float oldZPosition;
+    bool isDiscarded = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,8 @@ public class Card : MonoBehaviour
 
     private void OnMouseEnter()
     {
+        if (isDiscarded)
+            return;
         Vector3 v = transform.localPosition;
         oldZPosition = v.z;
         this.transform.localPosition = new Vector3(v.x, v.y, focusZPosition);
@@ -21,20 +25,26 @@ public class Card : MonoBehaviour
 
     private void OnMouseExit()
     {
-
+        if (isDiscarded)
+            return;
         Vector3 v = transform.localPosition;
         this.transform.localPosition = new Vector3(v.x, v.y, oldZPosition);
     }
 
     private void OnMouseUpAsButton()
     {
-        this.transform.SetParent(GameObject.FindGameObjectWithTag("Deck").gameObject.transform);
-        this.transform.localPosition = new Vector3(0, 0, 0);
+        if (isDiscarded)
+            return;
+        GameObject go = GameObject.FindGameObjectWithTag("Discard");
+        Vector3 v = new Vector3(0, 0, -cards);
+        this.transform.SetParent(go.transform, false);
+        isDiscarded = true;
+        this.transform.localPosition = v;
+        cards++;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
     }
 }
