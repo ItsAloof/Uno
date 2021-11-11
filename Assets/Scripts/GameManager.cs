81ef617765b313fd22630bab265b9ef32d211c5e
Nick <nickyrick@gmail.com>
2021-11-10 19:19:19 -0500

    Added some networking
    Started new branch for working on network without breaking main branch

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,8 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Photon.Pun;
 
-public class Game : MonoBehaviour
+public class GameManager : MonoBehaviourPunCallbacks, IPunObservable
 {
     [Tooltip("Card Prefab")]
     [SerializeField]
@@ -12,40 +13,47 @@ public class Game : MonoBehaviour
     [SerializeField]
     public Sprite[] Sprites;
 
+    [Tooltip("The local players deck")]
     [SerializeField]
     GameObject localPlayer;
 
 
 
-    List<GameObject> cards = new List<GameObject>();
+    static List<GameObject> cards = new List<GameObject>();
+    List<int> taken = new List<int>();
 
     // Start is called before the first frame update
     void Start()
     {
-        Camera.main.depthTextureMode = DepthTextureMode.Depth;
-        generateCards();
-    }
-    List<int> taken = new List<int>();
+        //Camera.main.depthTextureMode = DepthTextureMode.Depth;
 
+        if (!PhotonNetwork.IsMasterClient)
+        {
+            generateCards();
+        }
+    }
+
+    [PunRPC]
     void generateCards()
     {
-        for(int i = 0; i < 7; i++)
+        for (int i = 0; i < 7; i++)
         {
             GameObject cardGo = Instantiate(cardPrefab, localPlayer.transform);
         redo:
-            int cI = Random.Range(0, 54);
+            int cI = Random.Range(0, Sprites.Length);
             if (taken.Contains(cI))
                 goto redo;
             cardGo.GetComponent<SpriteRenderer>().sprite = Sprites[cI];
             Vector3 v3 = cardGo.GetComponent<Transform>().position;
-            Vector3 newV3 = new Vector3(-200 + i*50, v3.y + 100, 0 - i*2);
+            Vector3 newV3 = new Vector3(-300 + i * 100, v3.y + 150, 0 - i * 2);
             cardGo.GetComponent<Transform>().localPosition = newV3;
             Debug.Log($"{cardGo.GetComponent<Transform>().localPosition}");
             cards.Add(cardGo);
         }
+        if(PhotonNetwork.pla)
     }
 
-    public 
+
 
     // Update is called once per frame
     void Update()
@@ -59,5 +67,19 @@ public class Game : MonoBehaviour
         //        Debug.Log($"{hit.collider.gameObject.name}");
         //    }
         //}
+    }
+
+    public void OnPhotonSerializeView(PhotonStream stream, PhotonMessageInfo info)
+    {
+        if (stream.IsWriting)
+        {
+            stream.SendNext(cards);
+            stream.SendNext(taken);
+        }
+        else
+        {
+            cards = (List<GameObject>)stream.ReceiveNext();
+            taken = (List<int>)stream.ReceiveNext();
+        }
     }
 }
